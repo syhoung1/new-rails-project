816c8dbf4ea33fb9990bafba2eb9013cde1e9b29
Steven <syhoung1@gmail.com>
2017-04-19 02:33:21 +0000

    crud

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,52 @@
+class WikisController < ApplicationController
+  def index
+    @wikis = Wiki.all
+  end
+
+  def create
+    @wiki = Wiki.new(wiki_params)
+    
+    if @wiki.save
+      redirect_to @wiki
+    else
+      render :new
+    end
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.update_attributes(wiki_params)
+    
+    if @wiki.save
+      redirect_to @wiki
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    
+    if @wiki.destroy
+      redirect_to action: :index
+    else
+      render :show
+    end
+  end
+  
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+  end
+end
