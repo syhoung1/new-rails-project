36fe3176414fbd6ad7b7fb9b4715f8543ba52fb0
Steven <syhoung1@gmail.com>
2017-04-19 16:03:24 +0000

    email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,20 +1,10 @@
-# ActionMailer::Base.smtp_settings = {
-#   :port           => ENV['MAILGUN_SMTP_PORT'],
-#   :address        => ENV['MAILGUN_SMTP_SERVER'],
-#   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-#   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-#   :domain         => 'tranquil-beyond-98006.herokuapp.com',
-#   :authentication => :plain,
-# }
-# ActionMailer::Base.delivery_method = :smtp
-
 if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '2525',
     authentication: :plain,
-    user_name: ENV['SENDGRID_NAME'],
+    user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: 'heroku.com',
     enable_starttls_auto: true
